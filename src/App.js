c7c013f1f56774b5bd27b5df7a8f62b67bde10f5
Dee Grant <WalterGrant@gmail.com>
2022-12-22 20:57:30 -0600

    set up app styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
-import {Brand, CTA, Navbar} from './components'
+import { Brand, CTA, Navbar } from './components';
 
 function App() {
   return (
-      <div>
-          <h1>GPT-3</h1>
+      <div className="App">
+          <div className="gradient__bg">
+              <Navbar></Navbar>
+              <Header></Header>
+          </div>
           <Brand></Brand>
-          <CTA></CTA>
-          <Navbar></Navbar>
-          <Blog></Blog>
-          <Features></Features>
-          <Footer></Footer>
-          <Header></Header>
-          <Possibility></Possibility>
           <WhatGPT3></WhatGPT3>
+          <Features></Features>
+          <Possibility></Possibility>
+          <CTA></CTA>
+          <Blog></Blog>
+          <Footer></Footer>
       </div>
   );
 }
