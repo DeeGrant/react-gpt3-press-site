3be2921476ed94a32d79281105ddf16e695dc774
Dee Grant <WalterGrant@gmail.com>
2023-01-26 16:56:21 -0600

    add cta skeleton

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,9 +2,11 @@ import './cta.css'
 
 const CTA = () => {
     return (
-        <div>
-            CTA
-        </div>
+        <section id="CTA" className="section__margin section__padding">
+            <a href="#early-access">Request Early Access to Get Started</a>
+            <h5>Register today & start exploring the endless possiblities.</h5>
+            <button>Get Started</button>
+        </section>
     );
 };
 
