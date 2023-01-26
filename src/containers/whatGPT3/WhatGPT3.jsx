c0eba890672637407b6f0671aa04732d6a8a907d
Dee Grant <WalterGrant@gmail.com>
2023-01-26 12:57:30 -0600

    add what-is section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,9 +2,30 @@ import './whatGPT3.css';
 
 const WhatGpt3 = () => {
     return (
-        <div>
-            What GPT3?
-        </div>
+        <section id="wgpt3" className="section__margin">
+            <div>
+                <h4>What is GPT-3?</h4>
+                <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+            </div>
+            <div className="explore">
+                <h2 className="gradient__text">The possibilities are beyond your imagination</h2>
+                <a href="#library">Explore the Library</a>
+            </div>
+            <div className="examples">
+                <section>
+                    <h4>Chatbots</h4>
+                    <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought.</p>
+                </section>
+                <section>
+                    <h4>Knowledge</h4>
+                    <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments.</p>
+                </section>
+                <section>
+                    <h4>Education</h4>
+                    <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments.</p>
+                </section>
+            </div>
+        </section>
     );
 };
 
