126c6e285f967fb29f5761ad7a75b3625ca51e8c
Dee Grant <WalterGrant@gmail.com>
2023-01-26 17:54:21 -0600

    add Blog and Article structure

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,15 @@
 import './article.css';
 
-const Article = () => {
+const Article = ({title, date, article_link, image_url, alt_text= ''}) => {
     return (
-        <div>
-            Article
-        </div>
+        <article>
+            <img src={image_url} alt={alt_text} />
+            <section>
+                <span>{date}</span>
+                <h3>{title}</h3>
+                <a href={article_link}>Read Full Article</a>
+            </section>
+        </article>
     );
 };
 
