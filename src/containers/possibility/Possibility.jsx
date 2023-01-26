9ed1087f189577c57e706d4b3cdf590c7bb53d1b
Dee Grant <WalterGrant@gmail.com>
2023-01-26 16:45:27 -0600

    add possibility skeleton

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,16 @@
 import './possibility.css';
+import possibility from '../../assets/possibility.png'
 
 const Possibility = () => {
     return (
-        <div>
-            Possibility
-        </div>
+        <section id="possibility" className="section__padding">
+            <img src={possibility} alt="person with vr headset"/>
+            <div>
+                <h2 className="gradient__text">The possibilities are beyond your imagination</h2>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <a href="#early-access">Request Early Access to Get Started</a>
+            </div>
+        </section>
     );
 };
 
