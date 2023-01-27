ca1d5b66fe0e82d725adbb3480744b27cf3543eb
Dee Grant <WalterGrant@gmail.com>
2023-01-26 18:10:10 -0600

    add footer structure

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,9 +2,44 @@ import './footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer>
+            <div>
+                <h2>Do you want to step in to the future before others</h2>
+                <button>Request Early Access</button>
+            </div>
+            <div>
+                <section>
+                    <h5></h5>
+                    <address></address>
+                </section>
+                <section>
+                    <h6>Links</h6>
+                    <ul>
+                        <li><a href="#">Overons</a></li>
+                        <li><a href="#">Social Media</a></li>
+                        <li><a href="#">Counters</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h6>Company</h6>
+                    <ul>
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h6>Get in Touch</h6>
+                    <ul>
+                        <li><address></address></li>
+                        <li><phone></phone></li>
+                        <li><email></email></li>
+                    </ul>
+                </section>
+            </div>
+            <div><span>Copyright</span></div>
+        </footer>
     );
 };
 
